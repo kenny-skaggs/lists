7ce40e80b5f6f1f38e90bfa2fff88c5efc2b07ca
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2022-01-10 11:51:24 -0600

    setting up vue with buefy and darkly

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <b-field v-for='(item, index) in items' :key='index'>
+      <b-checkbox>
+        {{ item }}
+      </b-checkbox>
+    </b-field>
+    <button class="button is-primary">Primary</button>
+    <button class="button is-info">Info</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data() {
+    return {
+      items: [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+      ],
+    };
+  },
+};
+</script>
